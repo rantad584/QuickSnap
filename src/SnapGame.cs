24f1da362ed8e6a5d16a0cc21e6ee637c820cc6d
rantad584 <michaelliu017@gmail.com>
2019-03-19 23:20:30 +1100

    fixed SnapGame.cs

--- src/SnapGame.cs
+++ src/SnapGame.cs
@@ -14,52 +14,39 @@ namespace CardGames
 			SwinGame.LoadFontNamed ("GameFont", "Chunkfive.otf", 12);		//add new font into the game
         }
 
-		/// <summary>
-		/// Respond to the user input -- with requests affecting myGame
-		/// </summary>
-		/// <param name="myGame">The game object to update in response to events.</param>
-		private static void HandleUserInput(Snap myGame)
-		{
-			//Fetch the next batch of UI interaction
-			SwinGame.ProcessEvents();
-
-			if (SwinGame.KeyTyped (KeyCode.vk_SPACE))
-			{
-				myGame.FlipNextCard ();     
-			}
-
-			if (myGame.IsStarted)
-			{
-				if ( SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
-				{
-					//TODO: add sound effects
-				}
-				else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT))
-				{
-					myGame.PlayerHit(0);
-				}
-				else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT))
-				{
-					myGame.PlayerHit (1);
-				}
-			}
-
-            if (myGame.IsStarted) 
+        /// <summary>
+        /// Respond to the user input -- with requests affecting myGame
+        /// </summary>
+        /// <param name="myGame">The game object to update in response to events.</param>
+        private static void HandleUserInput(Snap myGame)
+        {
+            //Fetch the next batch of UI interaction
+            SwinGame.ProcessEvents();
+            if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
             {
-                if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT) && SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) {
-
-                } 
-
-                else if (SwinGame.KeyTyped (KeyCode.vk_LSHIFT)) {
-                    myGame.PlayerHit (0);
-                } 
-
-                else if (SwinGame.KeyTyped (KeyCode.vk_RSHIFT)) { 
-                    myGame.PlayerHit (1);
-                    }
+                myGame.Start();
+            }
+            if (SwinGame.KeyTyped(KeyCode.vk_SPACE))
+            {
+                myGame.FlipNextCard();
             }
-		}
 
+            if (myGame.IsStarted)
+            {
+                if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT) && SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    //TODO: add sound effects
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_LSHIFT))
+                {
+                    myGame.PlayerHit(0);
+                }
+                else if (SwinGame.KeyTyped(KeyCode.vk_RSHIFT))
+                {
+                    myGame.PlayerHit(1);
+                }
+            }
+        }
 		/// <summary>
 		/// Draws the game to the Window.
 		/// </summary>
